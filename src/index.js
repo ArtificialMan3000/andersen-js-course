b77f2f1a5624550b0c0492a5181f3f9f2b36a403
Aleksandr <legionerab3000@yandex.ru>
2021-08-25 00:44:13 +0300

    Solved task3 in array-practice

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@
 
 import any from './array-practice/task1';
 import arrayDiff from './array-practice/task2';
+import forEachRight from './array-practice/task3';
 
 // import { task1Old, task1New } from './es-features/task1';
 // import { task2Old, task2New } from './es-features/task2';
@@ -43,6 +44,10 @@ console.log('=============');
 
 console.log(arrayDiff([1, 2, 3], [1, 2, 4]));
 console.log(arrayDiff([1, 3, 3, 4], [1, 3, '4']));
+
+console.log('=============');
+
+forEachRight([1, 2, 3, 4], val => console.log(val));
 
 // console.log('task1 old', task1Old(10) === 500); // true
 // console.log('task1 new', task1New(10) === 500); // true
