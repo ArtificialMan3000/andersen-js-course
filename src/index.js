650180defc6d2a8bd8470ad7c157a967df8b9dbb
Aleksandr <legionerab3000@yandex.ru>
2021-08-25 01:35:23 +0300

    Solved task7 in array-practice

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ import forEachRight from './array-practice/task3';
 import union from './array-practice/task4';
 import createGenerator from './array-practice/task5';
 import transformArrayToNumber from './array-practice/task6';
+import take from './array-practice/task7';
 
 // import { task1Old, task1New } from './es-features/task1';
 // import { task2Old, task2New } from './es-features/task2';
@@ -74,6 +75,11 @@ console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc + item, 10))
 console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc * item));
 console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc * item, 1));
 console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc - item));
+
+console.log('=============');
+
+console.log(take([1, 2, 3], 5));
+console.log(take([1, 2, 3], 2));
 
 console.log('=============');
 
