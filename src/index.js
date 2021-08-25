0b262f3c45e6fbe29f92fb59be2383f2d9564b3d
Aleksandr <legionerab3000@yandex.ru>
2021-08-25 14:17:59 +0300

    Solved task10 in array-practice

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ import transformArrayToNumber from './array-practice/task6';
 import take from './array-practice/task7';
 import without from './array-practice/task8';
 import indexOfAll from './array-practice/task9';
+import membersOnActiveMeetups from './array-practice/task10';
 
 // import { task1Old, task1New } from './es-features/task1';
 // import { task2Old, task2New } from './es-features/task2';
@@ -94,6 +95,14 @@ console.log(indexOfAll([1, 2, 3, 1, 2, 3], 1));
 console.log(indexOfAll([1, 2, 3], 4));
 
 console.log('=============');
+
+const meetups = [
+  { name: 'JavaScript', isActive: true, members: 100 },
+  { name: 'Angular', isActive: true, members: 900 },
+  { name: 'Node', isActive: false, members: 600 },
+  { name: 'React', isActive: true, members: 500 },
+];
+console.log(membersOnActiveMeetups(meetups));
 
 // console.log('task1 old', task1Old(10) === 500); // true
 // console.log('task1 new', task1New(10) === 500); // true
