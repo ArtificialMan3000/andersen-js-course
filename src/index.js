d09dd4dd3969bea8fa7d33cb450ca22b94168229
Aleksandr <legionerab3000@yandex.ru>
2021-09-01 12:42:19 +0300

    Add task2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import './styles/main.css';
 import { foo, createCb } from './asynchronous-programming/task1';
+import { parseJSON, successCb, failureCb } from './asynchronous-programming/task2';
 
 foo(5, createCb('cb'));
 foo(20, createCb('cb'));
 
 console.log('=======');
+
+parseJSON('{"x": 10}', successCb, failureCb);
+parseJSON('{"x"}', successCb, failureCb);
+
 console.log('=======');
 console.log('=======');
 console.log('=======');
