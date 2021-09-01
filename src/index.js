dd7fd51f5ce54e15addcc5b7fbb9acd0e4599abe
Aleksandr <legionerab3000@yandex.ru>
2021-09-01 18:33:08 +0300

    Add task6

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import './styles/main.css';
 // import { parseJSON, successCb, failureCb } from './asynchronous-programming/task2';
 // import { delay } from './asynchronous-programming/task3';
 // import { getUserData } from './asynchronous-programming/task4';
-import { getData } from './asynchronous-programming/task5';
+// import { getData } from './asynchronous-programming/task5';
+import { getResolvedPromise } from './asynchronous-programming/task6';
 
 // foo(5, createCb('cb'));
 // foo(20, createCb('cb'));
@@ -15,21 +16,35 @@ import { getData } from './asynchronous-programming/task5';
 
 // getUserData();
 
-getData([
-  'https://random-data-api.com/api/address/random_address',
-  'https://random-data-api.com/api/appliance/random_appliance',
-  'https://random-data-api.com/api/app/random_app',
-  'https://random-data-api.com/api/bank/random_bank',
-  'https://random-data-api.com/api/beer/random_beer',
-]);
+// getData([
+//   'https://random-data-api.com/api/address/random_address',
+//   'https://random-data-api.com/api/appliance/random_appliance',
+//   'https://random-data-api.com/api/app/random_app',
+//   'https://random-data-api.com/api/bank/random_bank',
+//   'https://random-data-api.com/api/beer/random_beer',
+// ]);
 
-getData(
-  [
-    'https://random-data-api.com/api/address/random_address',
-    'https://random-data-api.com/api/appliance/random_appliance',
-    'https://random-data-api.com/api/app/random_app',
-    'https://random-data-api.com/api/bank/random_bank',
-    'https://random-data-api.com/api/beer/random_beer',
-  ],
-  false
-);
+// getData(
+//   [
+//     'https://random-data-api.com/api/address/random_address',
+//     'https://random-data-api.com/api/appliance/random_appliance',
+//     'https://random-data-api.com/api/app/random_app',
+//     'https://random-data-api.com/api/bank/random_bank',
+//     'https://random-data-api.com/api/beer/random_beer',
+//   ],
+//   false
+// );
+
+getResolvedPromise(500)
+  .then(value => {
+    try {
+      if (value > 300) {
+        throw Error('Ошибка');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  })
+  .finally(() => {
+    console.log('This is Finally!');
+  });
