f1f566b3fbd9ce204c8c32eaf6c4fbc6a6d0be39
Aleksandr <legionerab3000@yandex.ru>
2021-09-01 12:49:13 +0300

    Add task3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/main.css';
 import { foo, createCb } from './asynchronous-programming/task1';
 import { parseJSON, successCb, failureCb } from './asynchronous-programming/task2';
+import { delay } from './asynchronous-programming/task3';
 
 foo(5, createCb('cb'));
 foo(20, createCb('cb'));
@@ -11,11 +12,13 @@ parseJSON('{"x": 10}', successCb, failureCb);
 parseJSON('{"x"}', successCb, failureCb);
 
 console.log('=======');
-console.log('=======');
-console.log('=======');
-console.log('=======');
-console.log('=======');
-console.log('=======');
-console.log('=======');
-console.log('=======');
-console.log('=======');
+
+delay(1000).then(v => console.log(`Done with ${v}`));
+// console.log('=======');
+// console.log('=======');
+// console.log('=======');
+// console.log('=======');
+// console.log('=======');
+// console.log('=======');
+// console.log('=======');
+// console.log('=======');
